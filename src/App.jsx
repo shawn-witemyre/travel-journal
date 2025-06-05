d7a007e3554de2a1e08d2da213c2e972fde7c084
Shawn Witemyre <switemyre@comcast.net>
2025-06-05 18:33:08 -0400

    Created App component and rendered it

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Header from "./components/Header";
 export default function App() {
   return (
     <>
-      <h1>App</h1>
       <Header />
+      <h1>App</h1>
     </>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-/**
- * Challenge:
- *
- * - Create an App component in a separate file.
- *   Import it here and render it
- * - Create a "components" folder and a Header component.
- *   Render the Header inside the App component.
- * - Follow the Travel Journal design to build the Header
- *   for our page.
- */
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
